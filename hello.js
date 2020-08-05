2e817f39b160173c119f739d4dde58b3c4327f8f
shanti-s <64836823+shanti-s@users.noreply.github.com>
2020-08-05 14:00:34 +0200

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,14 @@
+/* logging texts in JavaScript! 
+    We've used parenthesis */
+
+console.log("Hello World!"); // This is a JS Statement
+console.log("Hello World Again!"); // Statements always ends with semicolon;
+// () parenthesis
+// {} curly-braces
+// [] square-brackets
+
+// concatenation-- It has nothing to do with Mathematical expressions
+console.log("Hello" + "World");
+console.log("2" + "3");
+console.log("Good morning" + "class" + 32);// 32 is a number here not variable 
+
